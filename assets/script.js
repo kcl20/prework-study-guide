648ac20cd12fa0fd5895dcdfc70e35edf364236a
Kenny Li <112835000+kcl20@users.noreply.github.com>
2022-10-16 12:46:11 -0400

    Added prework JS exercises.
    Added prework JS exercises.

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,28 @@
 var topic = "HTML";
 
 if (topic === 'HTML') {
-  console.log("Let's study HTML!");
+ console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
+ console.log("Let's study CSS!");
 } else if (topic === 'Git') {
-  console.log("Let's study Git!");
+ console.log("Let's study Git!");
 } else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
+ console.log("Let's study JavaScript!");
 } else {
-  console.log('Please try again!');
+ console.log('Please try again!');
 }
+
+console.log(topic)
+
+
+console.log(x === undefined); // true
+var x = 3;
+
+
+var shapes = ["Lu", 54, true]
+console.log(shapes[0])
+
+
+for(var x = 0; x < shapes.length; x++) {
+  console.log(shapes[x])
+ }
